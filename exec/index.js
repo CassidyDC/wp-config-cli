0b06a156e364cb95d923f5f4db38bf694f262234
Jacob Cassidy <jacob@jacobcassidy.com>
2025-02-08 10:21:50 +0700

    Add exports

---
+++ exec/index.js
@@ -0,0 +1,5 @@
+export { execDebug } from './debug.js';
+export { execHelp } from './help.js';
+export { execInstall } from './install.js';
+export { execLaunch } from './launch.js';
+export { execVersion } from './version.js';
