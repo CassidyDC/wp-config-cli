5a01f3823e4e5e394b8fb63627ac61ea7782c2c4
Jacob Cassidy <jacob@jacobcassidy.com>
2025-02-08 09:42:34 +0700

    Refactor `process.js`

--- utils/process.js
+++ utils/process.js
@@ -2,51 +2,68 @@
  * @module utils/process
  */
 
-import chalk from 'chalk';
-
 import { alert } from './alerts.js';
 import { cli } from './cli.js';
-import { header } from './header.js';
-import { execInstall, execLaunch } from '../commands/index.js';
+import { argv, inputCommands, inputFlags, s } from './constants.js';
+
+const commands = cli.commands;
+const flags = cli.flags;
+const allowedCommands = cli.allowedCommands;
+const allowedFlags = cli.allowedFlags;
 
 export function processArgs() {
-  const args = process.argv.slice(2);
-  const input = cli.input;
-
-  // Print error if more than one command is provided.
-  if (input.length > 1) {
-    alert('error', 'Too many commands provided. See `wp-config help` for a list of available commands.');
-    process.exit(1);
+  // Print help info if help command/flag is provided, or no command/flag is provided.
+  if (argv.length === 0 || inputCommands.includes('help') || inputFlags['help'] || inputFlags['h']) {
+    flags.help.exec();
   }
 
-  // Show Help
-  if (args.length === 0 || input.includes('help')) {
-    header(cli.flags.clear);
-    cli.showHelp(0);
-  }
-
-  // Run install command
-  (input.includes('install') || input.includes('i')) && execInstall();
-
-  // Run launch command
-  (input.includes('launch') || input.includes('l')) && execLaunch();
-
-  // Print warning if flag is provided without a command.
-  if (args.includes('--clear') || args.includes('--no-clear')) {
-    const command = args.includes('--clear') ? '--clear' : '--no-clear';
+  // Print an error if more than one command is provided.
+  if (inputCommands.length > 1) {
     alert(
-      'warning',
-      `The ${chalk.italic(command)} flag was used without a command. See \`wp-config help\` for a list of available commands.`,
+      'error',
+      `Too many commands provided. See ${s.green.italic('wp-config help')} for a list of available commands.`,
     );
     process.exit(1);
   }
 
-  // Print error if unknown command or unknown standalone flag is provided.
-  if (input.length > 0) {
-    alert('error', 'Unknown command provided. See `wp-config help` for a list of available commands.');
-    process.exit(1);
-  } else {
-    alert('error', 'Unknown flag provided. See `wp-config help` for a list of available flags.');
+  // Print an error if an unknown command is provided.
+  if (inputCommands.length === 1 && !allowedCommands.includes(inputCommands[0])) {
+    alert('error', `Unknown command used. See ${s.green.italic('wp-config help')} for a list of available commands.`);
     process.exit(1);
   }
+
+  // Print an error if an unknown flag is provided.
+  if (inputFlags.length > 0) {
+    inputFlags.forEach((flag) => {
+      if (!allowedFlags.includes(flag)) {
+        alert('error', `Unknown flag used. See ${s.green.italic('wp-config help')} for a list of available options.`);
+        process.exit(1);
+      }
+    });
+  }
+
+  // Print debug info if a debug flag is provided.
+  if (inputFlags.includes('debug') || inputFlags.includes('d')) {
+    flags.debug.exec();
+    process.exit(0);
+  }
+
+  // Execute command if valid.
+  if (inputCommands.length === 1 && allowedCommands.includes(inputCommands[0])) {
+    const cmd = commands[inputCommands[0]];
+
+    if (cmd) {
+      commands[cmd].exec();
+    } else {
+      // Get command by alias.
+      Object.keys(commands).forEach((command) => {
+        if (commands[command].alias === inputCommands[0]) {
+          commands[command].exec();
+        }
+      });
+    }
+    process.exit(0);
+  }
+
+  alert('warning', 'Process not caught by any condition.');
 }
