8ef8dbeac477f182e06b6219409590af7a532cad
Jacob Cassidy <jacob@jacobcassidy.com>
2025-02-09 07:07:31 +0700

    Include `runInstallPrompts()`

--- exec/install.js
+++ exec/install.js
@@ -2,15 +2,26 @@
  * @module commands/install
  */
 
-import { alert, clearConsole, printHeader } from '../utils/index.js';
+import { alert, clearConsole, printHeader, runInstallPrompts } from '../utils/index.js';
 
 /**
  * Runs the installation process.
  *
  * @return {void}
  */
-export function execInstall() {
+export async function execInstall() {
   clearConsole();
   printHeader();
-  alert('heading', 'Running...', 'Installation');
+  alert('heading', 'Running...', 'Install');
+
+  const userInputs = await runInstallPrompts();
+  console.log(userInputs);
+
+  return new Promise((resolve, reject) => {
+    if (userInputs) {
+      resolve(userInputs);
+    } else {
+      reject(new Error('Failed to load input'));
+    }
+  });
 }
